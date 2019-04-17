aab42ecb3be57775db55d233c646dc90b23fee57
Artem Golubev <pety3bi@users.noreply.github.com>
2019-04-17 15:12:48 +0300

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,7 +2,7 @@
 
 sslh -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
    --ssh $SSH_HOST:$SSH_PORT \
-   --ssl $HTTPS_HOST:$HTTPS_PORT \
+   --tls $HTTPS_HOST:$HTTPS_PORT \
    --openvpn $OPENVPN_HOST:$OPENVPN_PORT \
    --socks5 $SOCKS_HOST:$SOCKS_PORT \
    --anyprot $MTPROTO_HOST:$MTPROTO_PORT \
