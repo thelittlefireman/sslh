0971875c8bbab361acb5c2ea03a26889a22580b3
thelittlefireman <thelittlefireman@users.noreply.github.com>
2020-03-26 18:07:44 +0100

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,7 +3,5 @@
 sslh -f -u root --listen $LISTEN_IP:$LISTEN_PORT \
    --ssh $SSH_HOST:$SSH_PORT \
    --tls $HTTPS_HOST:$HTTPS_PORT \
-   --openvpn $OPENVPN_HOST:$OPENVPN_PORT \
-   --socks5 $SOCKS_HOST:$SOCKS_PORT \
-   --anyprot $MTPROTO_HOST:$MTPROTO_PORT \
+   --openvpn $OPENVPN_HOST:$OPENVPN_PORT
    $@
